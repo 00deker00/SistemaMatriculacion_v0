4dd16fa8b7250988d6b3e004c480295aae0c085d
sergi <00DEKER00@GMAIL.COM>
2025-03-03 19:52:59 +0100

    CREADA LA CLASE MODALIDAD

--- src/main/java/org/iesalandalus/programacion/matriculacion/modelo/dominio/Modalidad.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/dominio/Modalidad.java
@@ -1,4 +1,28 @@
 package org.iesalandalus.programacion.matriculacion.modelo.dominio;
 
 public enum Modalidad {
+    PRESENCIAL("Presencial"),
+    SEMIPRESENCIAL("Semipresencial");
+
+    private final String cadenaAMostrar;
+
+    private Modalidad (String cadenaAMostrar){
+        this.cadenaAMostrar = cadenaAMostrar;
+    };
+
+    //ORDINAL DEVUELVE LAS OPCIONES CON UN NÚMERO EMPEZANDO DESDE 0
+    //PONER +1?
+    public String imprimir() {
+        return ordinal() + ".-" + cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Modalidad{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+
+
+
 }
